98a1645189ed9990c0266d37a66774c680d5b834
Anna Andreeva <78911498+andreeva2909@users.noreply.github.com>
2021-12-05 02:44:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-import http from 'http';
-const CORS = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET,POST,DELETE,OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, x-test'
-  };
-const server = http.createServer(async(req,res)=>{
-      if(req.url === '/result4/'){
-          
-          res.writeHead(200,{
-              'Content-Type':'application/json',
-              ...CORS,
-          })
-          let data = '';
-          await req.on('data', function(chunk){
-            data += chunk;
-            }).on('end', () => {
-          })
-          res.write(JSON.stringify({
-            "message":"itmo307691",
-            "x-result":headers,
-            "x-body":data
-            }
-            ))
-      }
-      res.end()
-      
-  });
-  server.listen(4321,()=>{
-      console.log('Server is running')});
+const http = require('http');
+
+http
+  .Server((req, res) => {
+
+    if (req.url === '/result4/') {
+      let CORS = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,POST,DELETE,OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, x-test'
+
+      };
+
+      const result = {
+        message: 'itmo307691',
+        'x-result': req.headers['x-test'],
+      };
+      let body = '';
+
+      req
+        .on('data', data => (body += data))
+        .on('end', () => {
+          result['x-body'] = body;
+          res.writeHead(200, {... CORS, 'Content-Type': 'application/json' });
+          res.end(JSON.stringify(result));
+        });
+    }
+  })
+  .listen(process.env.PORT);
